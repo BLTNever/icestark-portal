01396519fefb35e7defd64d003867a5adfa34bb2
Alec <alec_000@163.com>
2020-04-16 19:21:14 +0800

    update

--- src/layouts/BasicLayout/menuConfig.ts
+++ src/layouts/BasicLayout/menuConfig.ts
@@ -17,78 +17,19 @@ const asideMenuConfig = [
         icon: 'account',
     },
     {
-        name: 'Goods',
+        name: 'child',
         icon: 'set',
         children: [
             {
-                path: '/',
+                path: 'child/',
                 name: 'home',
             },
             {
-                path: '/master',
+                path: 'child/master',
                 name: 'master',
             },
         ]
     },
-    {
-        name: '商家平台',
-        icon: 'atm',
-        children: [
-            {
-                path: '/seller',
-                name: 'home',
-            },
-            {
-                path: '/seller/master',
-                name: 'master',
-            },
-            // {
-            //     path: '/seller/detail',
-            //     name: '商家详情',
-            // },
-            // {
-            //     path: '/seller/404',
-            //     name: '商家 404',
-            // },
-        ],
-    },
-    {
-        name: 'manifest',
-        icon: 'account',
-        path: '/local/',
-        // children: [
-        //     {
-        //         path: '/local/master',
-        //         name: 'master',
-        //     },
-        //     {
-        //         path: '/local/',
-        //         name: 'home',
-        //     },
-        // ]
-    },
-    // {
-    //     name: '小二平台',
-    //     icon: 'account',
-    //     children: [
-    //         {
-    //             path: '/waiter',
-    //             name: '小二首页',
-    //         },
-    //         {
-    //             path: '/waiter/list',
-    //             name: '小二列表',
-    //         },
-    //         {
-    //             path: '/waiter/detail',
-    //             name: '小二详情',
-    //         },
-    //         {
-    //             path: '/waiter/404',
-    //             name: '小二 404',
-    //         },
-    //     ],
-    // },
 ];
 
 export { headerMenuConfig, asideMenuConfig };
